acae47179cc79d3d4b30360bea08b9edb00a31a7
Aleksa Rajković <aleksa.rajkovic@nutanix.com>
2022-09-22 10:35:29 +0200

    Format

--- apps/animals/lib/animals/descriptor.ex
+++ apps/animals/lib/animals/descriptor.ex
@@ -1,10 +1,8 @@
-
 defmodule Animals.Behaviour do
   @callback speak() :: String.t()
   @callback type() :: atom()
   @callback forgot_to_specify_default() :: any
   @optional_callbacks [speak: 0, type: 0, forgot_to_specify_default: 0]
-
 
   def default_speak do
     "lele!"
@@ -14,33 +12,35 @@ defmodule Animals.Behaviour do
     :lele
   end
 
-
   defmacro inspect_callbacks(env) do
     Enum.reduce(
       @optional_callbacks,
       [],
       &Keyword.merge(&1, &2)
-    )|>
-    Enum.filter(
-      fn {callback, arrity} -> not Module.defines?(env.module, {callback, arrity}, :def) end
-    )|>
-    Enum.map(
-      fn {callback, _arrity} ->
-        try do
-          default_callback_name = "default_#{callback}" |> String.to_existing_atom
-          quote do
-            def unquote(callback)(), do: Kernel.apply(Animals.Behaviour, unquote(default_callback_name), [])
-          end
-        rescue ArgumentError ->
-          quote do
-            def unquote(callback)(), do: raise ArgumentError, message: "Default handler for #{unquote(callback)} not defined"
-          end
-        end
-      end
     )
+    |> Enum.filter(fn {callback, arrity} ->
+      not Module.defines?(env.module, {callback, arrity}, :def)
+    end)
+    |> Enum.map(fn {callback, _arrity} ->
+      try do
+        default_callback_name = "default_#{callback}" |> String.to_existing_atom()
 
+        quote do
+          def unquote(callback)(),
+            do: Kernel.apply(Animals.Behaviour, unquote(default_callback_name), [])
+        end
+      rescue
+        ArgumentError ->
+          quote do
+            def unquote(callback)(),
+              do:
+                raise(ArgumentError,
+                  message: "Default handler for #{unquote(callback)} not defined"
+                )
+          end
+      end
+    end)
   end
-
 end
 
 defmodule Animals.Descriptor do
@@ -52,5 +52,4 @@ defmodule Animals.Descriptor do
       def describe(), do: "#{type()} says #{speak()}"
     end
   end
-
 end
